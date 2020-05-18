9ad0707990b6740b0e0e7255bd2eaa3f80f541d0
Phuong Le <Phuong.Le@altsrc.net>
2020-05-18 18:33:25 +0700

    Mastering Vuex Lesson 5

--- src/store.js
+++ src/store.js
@@ -1,10 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    user: { id: '1233', name: 'ABC' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: []
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event.data)
+      })
+    }
+  }
 })
