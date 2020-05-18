83a19b2cbab403c15097096a4c6cc216ffb9eaf7
PhuongLe <lethanhphuong17051989@gmail.com>
2020-05-18 08:10:47 +0700

    completed

--- src/router.js
+++ src/router.js
@@ -1,21 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import EventList from './views/EventList.vue'
+import EventCreate from './views/EventCreate.vue'
+import EventShow from './views/EventShow.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'event-list',
+      component: EventList
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About
+      path: '/event/create',
+      name: 'event-create',
+      component: EventCreate
+    },
+    {
+      path: '/event/:id',
+      name: 'event-show',
+      component: EventShow,
+      props: true
     }
   ]
 })
